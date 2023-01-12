f54a0c733c10fb3bc30b660e1a95a2748fbe1f37
B0222278 <B0222278@LTB0222278.india.airtel.itm>
2023-01-12 12:11:48 +0530

    wait commit

--- Selenium4Project/src/main/java/test/Waits.java
+++ Selenium4Project/src/main/java/test/Waits.java
@@ -2,10 +2,10 @@ package test;
 
 import java.time.Duration;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Waits {
@@ -14,23 +14,14 @@ public class Waits {
 		
 				WebDriverManager.chromedriver().setup();
 				WebDriver driver = new ChromeDriver();
-			
+				
+				
 				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 				
 				//Open a Webpage
-				driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+				driver.get("https://google.com");
 				
-				//JS alert
-				driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
-				Alert alert1 = driver.switchTo().alert();
-				System.out.println(alert1.getText());
-				Thread.sleep(4000);
-				alert1.accept();
-				
-				if(driver.getPageSource().contains("You successfully clicked an alert"))
-					System.out.println("You successfully clicked an alert");
-				System.out.println("=======================================");
-				
+			
 				Thread.sleep(4000);
 				driver.close();
 				driver.quit();

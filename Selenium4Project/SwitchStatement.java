5ec46321fb41ea29b00f8dae2627e42066633ed8
Amar Chaudhary <40181857+AmarScripts@users.noreply.github.com>
2023-08-28 23:10:45 +0530

    Create SwitchStatement.java

---
+++ Selenium4Project/SwitchStatement.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+		
+/*		
+		int passcode = 629;
+		String coffeeType;
+		
+		switch(passcode){
+			
+			case 555 : coffeeType = "Espresso";
+			break;
+			case 312 : coffeeType = "Vanilla latte";
+			break;
+			case 629 : coffeeType = "Drip coffee";
+			break;
+			default : coffeeType = "unknown";
+			break;
+			
+		}
+		System.out.println(coffeeType);
+	}
+
+	*/
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the month number: ");
+		int month = input.nextInt();
+		//int month=7;
+		String monthString;
+		
+		switch(month) {
+		case 1: monthString = "January";
+		break;
+		case 2: monthString = "February";
+		break;
+		case 3: monthString = "March";
+		break;
+		case 4: monthString = "April";
+		break;
+		case 5: monthString = "May";
+		break;
+		case 6: monthString = "June";
+		break;
+		case 7: monthString = "July";
+		break;
+		case 8: monthString = "August";
+		break;
+		case 9: monthString = "September";
+		break;
+		case 10: monthString = "October";
+		break;
+		case 11: monthString = "November";
+		break;
+		case 12: monthString = "December";
+		break;
+		default : monthString = "Invalid month";
+		break;
+		}
+		System.out.println(monthString);
+
+}
+
+}

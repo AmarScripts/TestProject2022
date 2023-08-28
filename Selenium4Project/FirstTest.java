3a758caa0e75bc25dbda330ad1160997e3d7fb01
Amar Chaudhary <40181857+AmarScripts@users.noreply.github.com>
2023-08-28 22:58:18 +0530

    Create FirstTest.java

---
+++ Selenium4Project/FirstTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\B0222278\\\\Downloads\\\\chromedriver_win32 (1)\\\\chromedriver.exe");
+		//WebDriverManager.chromedriver().setup();
+		//WebDriverManager.chromedriver().driverVersion("108.0.5359.71").setup();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		//WebDriverManager.firefoxdriver().setup();
+		//WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.google.com");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		
+		TimeUnit.SECONDS.sleep(10);
+		
+		driver.close();
+
+	}
+
+}

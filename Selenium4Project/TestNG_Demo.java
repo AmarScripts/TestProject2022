82b7aa4da8f652e5b9c837b2e23fc3fca28fcbef
Amar Chaudhary <40181857+AmarScripts@users.noreply.github.com>
2023-09-28 10:10:58 +0530

    Create TestNG_Demo.java

---
+++ Selenium4Project/TestNG_Demo.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestNG_Demo {
+
+	private static WebDriver driver= null;
+	
+	@BeforeTest
+	public void setUpTest() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public static void googleSearch() {
+		
+		//Goto google.com
+		driver.get("https://google.com");
+		
+		//Enter text in search button
+		driver.findElement(By.name("q")).sendKeys("Automation Step by Step");
+		
+		//Click on search button
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+		
+	}
+	
+	@AfterTest
+	public void tearDownTest() {
+		
+		driver.close();    
+		driver.quit();
+		System.out.println("Test completed sucessfully");
+	}
+
+}
